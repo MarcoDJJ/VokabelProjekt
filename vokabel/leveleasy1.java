677969c167f2f1b9d9ddc39a8f7f4046647ce1a7
MarcoDJJ <47525244+MarcoDJJ@users.noreply.github.com>
2019-03-05 20:04:31 +0100

    edited leveleasy1.java zufalleasyy.java Resolved a bug that had zufalleasyy.java Now the programm can say if the translation is correct or not

--- vokabel/leveleasy1.java
+++ vokabel/leveleasy1.java
@@ -14,6 +14,9 @@ import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import java.util.Random;
 import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 /**
  *
  * Beschreibung
@@ -30,14 +33,26 @@ public class leveleasy1 extends JFrame {
   private JButton jButton1 = new JButton();
   private JTextField jTextField1 = new JTextField();
   private JTextField jTextField2 = new JTextField();
+  private JButton jButton2 = new JButton();
+  private JLabel jLabel4 = new JLabel();
   // Ende Attribute
   vokabelnarray vlevel11= new vokabelnarray();
-  vokabel f = new vokabel("f", "ff");
+  vokabel f1 = new vokabel("v", "vv");
   vokabelnarray vlevel12= new vokabelnarray();
-  vokabel f1 = new vokabel("g", "gg");
+  vokabel f = new vokabel("g", "gg");
   zufalleasyy zlevel1= new zufalleasyy();
   Timerclass tt;
   Timerclass timerlevel1 =new Timerclass(jLabel1);
+ 
+   /*public void keyPressed(KeyEvent e) {
+
+    int key = e.getKeyCode();
+
+    if(key == KeyEvent.VK_D){
+
+        stop();
+    }
+}*/
   
   public leveleasy1() { 
     // Frame-Initialisierung
@@ -70,35 +85,70 @@ public class leveleasy1 extends JFrame {
     jButton1.setMargin(new Insets(2, 2, 2, 2));
     jButton1.addActionListener(new ActionListener() { 
       public void actionPerformed(ActionEvent evt) { 
-        vlevel11.fuegevokabelnhinzu(f);
         vlevel12.fuegevokabelnhinzu(f1);
-        zlevel1.fuegevoccnhinzu(vlevel11);
+        vlevel11.fuegevokabelnhinzu(f);
         zlevel1.fuegevoccnhinzu(vlevel12);
-        jTextField1.setText(zlevel1.getVocc().getVokabelnA(0).getDeutsch());
+        zlevel1.fuegevoccnhinzu(vlevel11);
+        
+        
+        jTextField1.setText(zlevel1.getVocc().getVokabelnA(0).getEnglish());
+        
         timerlevel1.start();
       }
     });
+    
     cp.add(jButton1);
     jTextField1.setBounds(96, 96, 121, 33);
     cp.add(jTextField1);
     jTextField2.setBounds(96, 160, 113, 33);
     cp.add(jTextField2);
+    jButton2.setBounds(96, 40, 65, 33);
+    jButton2.setText("Verify");
+    jButton2.setMargin(new Insets(2, 2, 2, 2));
+    jButton2.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        if(jTextField2.getText().equals(zlevel1.ynow().getVokabelnA(0).getDeutsch())){
+          jLabel4.setText("Gut Gemacht!");
+          
+          jTextField1.setText(zlevel1.getVocc().getVokabelnA(0).getEnglish());
+          jTextField2.setText(null);
+        }
+        
+          else{ 
+          jLabel4.setText("Falsch");
+          
+          jTextField1.setText(zlevel1.getVocc().getVokabelnA(0).getEnglish());
+          jTextField2.setText(null); 
+        
+        }
+      
+    }
+    });
+    cp.add(jButton2);
+    jLabel4.setBounds(96, 216, 113, 33);
+    jLabel4.setText("");
+    cp.add(jLabel4);
     // Ende Komponenten
     
-    setVisible(true);
+  setVisible(true);
   } // end of public leveleasy1
   
   // Anfang Methoden
   
-  public static void main(String[] args) {
-    new leveleasy1();
+    public static void main(String[] args) {
+      new leveleasy1();
   } // end of main
   
-  public void jButton1_ActionPerformed(ActionEvent evt) {
+    public void jButton1_ActionPerformed(ActionEvent evt) {
     // TODO hier Quelltext einfügen
-    
+  
   } // end of jButton1_ActionPerformed
+  
+    public void jButton2_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+  
+  } // end of jButton2_ActionPerformed
 
-  // Ende Methoden
+// Ende Methoden
 } // end of class leveleasy1
 
--- vokabel/zufalleasyy.java
+++ vokabel/zufalleasyy.java
@@ -1,16 +1,29 @@
  import java.util.ArrayList;
 import java.util.Random;
 public class zufalleasyy {
-   Random voc= new Random();
-  int y= voc.nextInt(1);
+  Random voc= new Random();
+  int c=1;
+  int y= voc.nextInt(c);
+  int k=y;
   ArrayList<vokabelnarray> vocc = new ArrayList<>();
   public void fuegevoccnhinzu(vokabelnarray neu){
     vocc.add(neu);
   }
   public vokabelnarray getVocc() {
+   
+    y= voc.nextInt(c++);
+     this.k=y;
     return vocc.get(y);
+     
+    
   }
 
+  public vokabelnarray ynow(){
+    
+    return vocc.get(k);
+  }
+  public static void main(String[] args) {
+  } 
   // Ende Methoden
   
 }
